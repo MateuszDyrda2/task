77fd53e5ac540ddac35cb9ba785d47c423eda5e7
matzix <mateusz.dyrda2@gmail.com>
2021-10-24 00:12:00 +0200

    added test

--- test/test.cpp
+++ test/test.cpp
@@ -5,9 +5,18 @@
 using namespace eol;
 int main()
 {
-	task t([]
-		   { std::cout << "works\n"; });
+	task t[] = {[]
+				{ std::cout << "1\n"; },
+				[]
+				{ std::cout << "2\n"; },
+				[]
+				{ std::cout << "3\n"; },
+				[]
+				{ std::cout << "4\n"; },
+				[]
+				{ std::cout << "5\n"; }};
+	for (int i = 0; i < 5; ++i)
+		t[i]();
 
-	t();
 	return 0;
 }
