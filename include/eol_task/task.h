abdcdaa5dff59a72fc86245235a6aaa9e56afbe4
matzix <mateusz.dyrda2@gmail.com>
2021-11-07 16:23:10 +0100

    refactored and changed alignment

--- include/eol_task/task.h
+++ include/eol_task/task.h
@@ -5,67 +5,84 @@
 #include <utility>
 
 namespace eol {
+namespace task_detail {
+union callable_align
+{
+	void* a;
+	double b;
+	long long c;
+};
+template <std::size_t _Size>
+struct task_storage
+{
+	struct type
+	{
+		alignas(alignof(callable_align)) unsigned char data[_Size];
+
+		inline void* get() noexcept { return &data[0]; }
+		inline void const* get() const noexcept { return &data[0]; }
+
+		template <class F>
+		inline F& get() noexcept { return *static_cast<F*>(get()); }
+		template <class F>
+		inline F const& get() const noexcept { return *static_cast<F const*>(get()); }
+	};
+};
+template <std::size_t _Size>
+using task_storage_t = typename task_storage<_Size>::type;
+
+enum class operation { clone_fn,
+					   destroy_fn };
+template <class F, std::size_t _Size>
+struct handler
+{
+	using storage_t = task_storage_t<_Size>;
+
+	inline static F const& get_callable(storage_t const& src) noexcept
+	{
+		return src.template get<F>();
+	}
+	template <class Fn>
+	static void create_fn(storage_t& dst, Fn&& fn)
+	{
+		::new (dst.get()) F(std::forward<Fn>(fn));
+	}
+	static void destroy_fn(storage_t& dst)
+	{
+		dst.template get<F>().~F();
+	}
+	static void _invoke_(storage_t const& dst)
+	{
+		dst.template get<F>()();
+	}
+	static void _manage_(storage_t& dst, storage_t const& src, operation op)
+	{
+		switch (op)
+		{
+		case operation::clone_fn:
+			create_fn(dst, get_callable(src));
+			break;
+		case operation::destroy_fn:
+			destroy_fn(dst);
+			break;
+		default:
+			break;
+		}
+	}
+};
+
+} // namespace task_detail
 /** Task is the standard function implementation representing 
  * a function wiout a return type and taking no arguments created 
  * without dynamically allocating memory. It allows for specifying
  * the size and alignment of the task. 
 */
-template <std::size_t Size = 32, std::size_t Align = 16>
+template <std::size_t Size = 32>
 class task
 {
   private:
-	struct storage_t
-	{
-		inline void* get() noexcept { return &_storage; }
-		inline void const* get() const noexcept { return &_storage; }
-
-		template <class T>
-		inline T& get() noexcept { return *static_cast<T*>(get()); }
-		template <class T>
-		inline T const& get() const noexcept { return *static_cast<T const*>(get()); }
-
-		std::aligned_storage_t<Size, Align> _storage;
-	};
-
-	enum operation { clone_fn,
-					 destroy_fn };
-
-	template <class F>
-	struct manager_t
-	{
-		inline static F const& get_callable(storage_t const& src) noexcept
-		{
-			return src.get<F>();
-		}
-		template <class Fn>
-		static void create_fn(storage_t& dst, Fn&& fn)
-		{
-			::new (dst.get()) F(std::forward<Fn>(fn));
-		}
-		static void destroy_fn(storage_t& dst)
-		{
-			dst.get<F>().~F();
-		}
-		static void _invoke_(storage_t const& dst)
-		{
-			dst.get<F>()();
-		}
-		static void _manage_(storage_t& dst, storage_t const& src, operation op)
-		{
-			switch (op)
-			{
-			case operation::clone_fn:
-				create_fn(dst, get_callable(src));
-				break;
-			case operation::destroy_fn:
-				destroy_fn(dst);
-				break;
-			default:
-				break;
-			}
-		}
-	};
-	using manager_f = void (*)(storage_t&, storage_t const&, operation);
+	using storage_t = task_detail::task_storage_t<Size>;
+	using manager_f = void (*)(storage_t&, storage_t const&, task_detail::operation);
 	using invoker_f = void (*)(storage_t const&);
 
   public:
@@ -80,11 +97,12 @@ class task
 	template <class F>
 	task(F&& callable) requires(std::is_invocable_v<F> && !std::is_same_v<task, std::decay_t<F>>)
 	{
-		using handler = manager_t<std::decay_t<F>>;
+		using _Fn	  = std::decay_t<F>;
+		using handler_t = task_detail::handler<_Fn, Size>;
 
-		handler::create_fn(_storage, std::forward<std::decay_t<F>>(callable));
-		_manager = &handler::_manage_;
-		_invoker = &handler::_invoke_;
+		handler_t::create_fn(_storage, std::forward<_Fn>(callable));
+		_manager = &handler_t::_manage_;
+		_invoker = &handler_t::_invoke_;
 	}
 	/** @brief Copies the task stored in other
 	 * @param other task to be copied 
@@ -93,7 +111,7 @@ class task
 	{
 		if (other)
 		{
-			other._manager(_storage, other._storage, clone_fn);
+			other._manager(_storage, other._storage, task_detail::operation::clone_fn);
 			_manager = other._manager;
 			_invoker = other._invoker;
 		}
@@ -114,7 +132,7 @@ class task
 	}
 	~task()
 	{
-		_manager(_storage, _storage, operation::destroy_fn);
+		_manager(_storage, _storage, task_detail::operation::destroy_fn);
 	}
 	/** @brief Copy assigns the task 
 	 * @param other task to be copied
@@ -169,7 +187,7 @@ class task
 };
 /** Deduction guide for the task */
 template <class F>
-task(F&& callable) -> task<sizeof(F), alignof(F)>;
+task(F&& callable) -> task<sizeof(F)>;
 
 } // namespace eol
 #endif // !EOL_TASK_TASK_H
